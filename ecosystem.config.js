f1d1b8c09a82dd3e869cd6bc78afb650cafec5ac
amit-node-dev <amit.v.easternts@gmail.com>
2024-10-07 10:30:02 +0530

    Chnages 6

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -5,11 +5,9 @@ module.exports = {
       script: "./app.js",
       env: {
         NODE_ENV: "development",
-        APIKEY: process.env.APIKEY,
       },
       env_production: {
         NODE_ENV: "production",
-        OPENAI_API_KEY: process.env.OPENAI_API_KEY,
       },
     },
   ],
