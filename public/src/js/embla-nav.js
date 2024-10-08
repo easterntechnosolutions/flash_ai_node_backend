1cddb9d45541d94ea8a909ca75127020f3b8d03d
amit-node-dev <amit.v.easternts@gmail.com>
2024-10-08 13:46:18 +0530

    Re configuring the image carasoul in home page of the website

--- public/src/js/embla-nav.js
+++ public/src/js/embla-nav.js
@@ -1,7 +1,7 @@
 export const addDotBtnsAndClickHandlers = (
   emblaApi,
   dotsNode,
-	currentSlideCounterNode,
+  currentSlideCounterNode,
   onButtonClick
 ) => {
   let dotNodes = [];
@@ -9,46 +9,50 @@ export const addDotBtnsAndClickHandlers = (
   const addDotBtnsWithClickHandlers = () => {
     dotsNode.innerHTML = emblaApi
       .scrollSnapList()
-      .map(() => `
+      .map(
+        () => `
 				<button 
 					type="button"
 					class="
 						embla-nav__dot w-[70px] xs:w-[82px] h-1 xl:w-1 xl:h-[82px] bg-gray-500 data-[active=true]:bg-aqua-500 rounded-full 
-						transition-colors durarion-50
+						transition-colors duration-50
 					"
 					data-active="false"
 					aria-label="dot"
-				></button>
-			`)
-      .join('')
+				></button>`
+      )
+      .join("");
 
     const scrollTo = (index) => {
       emblaApi.scrollTo(index);
       if (onButtonClick) onButtonClick(emblaApi);
-    }
+    };
 
-    dotNodes = Array.from(dotsNode.querySelectorAll('.embla-nav__dot'))
+    dotNodes = Array.from(dotsNode.querySelectorAll(".embla-nav__dot"));
     dotNodes.forEach((dotNode, index) => {
-      dotNode.addEventListener('click', () => scrollTo(index), false);
-    })
-  }
+      dotNode.addEventListener("click", () => scrollTo(index), false);
+    });
+  };
 
   const toggleDotBtnsActive = () => {
-    const previous = emblaApi.previousScrollSnap()
-    const selected = emblaApi.selectedScrollSnap()
+    const previous = emblaApi.previousScrollSnap();
+    const selected = emblaApi.selectedScrollSnap();
     dotNodes[previous].dataset.active = false;
     dotNodes[selected].dataset.active = true;
-		if(currentSlideCounterNode) currentSlideCounterNode.innerText = `0${emblaApi.selectedScrollSnap() + 1}`;
-  }
+    if (currentSlideCounterNode)
+      currentSlideCounterNode.innerText = `0${
+        emblaApi.selectedScrollSnap() + 1
+      }`;
+  };
 
   emblaApi
-    .on('init', addDotBtnsWithClickHandlers)
-    .on('reInit', addDotBtnsWithClickHandlers)
-    .on('init', toggleDotBtnsActive)
-    .on('reInit', toggleDotBtnsActive)
-    .on('select', toggleDotBtnsActive)
+    .on("init", addDotBtnsWithClickHandlers)
+    .on("reInit", addDotBtnsWithClickHandlers)
+    .on("init", toggleDotBtnsActive)
+    .on("reInit", toggleDotBtnsActive)
+    .on("select", toggleDotBtnsActive);
 
   return () => {
-    dotsNode.innerHTML = ''
-  }
-}
+    dotsNode.innerHTML = "";
+  };
+};
--- public/src/js/main.js
+++ public/src/js/main.js
@@ -1,66 +1,108 @@
-import EmblaCarousel from 'embla-carousel';
-import Autoplay from 'embla-carousel-autoplay';
-import { addDotBtnsAndClickHandlers } from '/src/js/embla-nav';
-import { listen } from '/src/js/utils';
-import { enter, leave } from '/src/js/transition';
-import SwapAnimation from '/src/js/animations/SwapAnimation';
-import FlowAnimation from '/src/js/animations/FlowAnimation';
+import EmblaCarousel from "embla-carousel";
+import Autoplay from "embla-carousel-autoplay";
 
-const emblaNode = document.querySelector('#embla');
-const dotsNode = document.querySelector('#embla-nav__dots');
-const currentSlideCounterNode = document.querySelector('#embla-nav__current');
+import { addDotBtnsAndClickHandlers } from "../js/embla-nav";
+import { listen } from "../js/utils";
+import { enter, leave } from "../js/transition";
+import SwapAnimation from "../js/animations/SwapAnimation";
+import FlowAnimation from "../js/animations/FlowAnimation";
 
-const options = { axis: "x", loop: true }
+const emblaNode = document.querySelector("#embla");
+const dotsNode = document.querySelector("#embla-nav__dots");
+const currentSlideCounterNode = document.querySelector("#embla-nav__current");
+
+const options = { axis: "x", loop: true };
 
 const emblaApi = EmblaCarousel(emblaNode, options, [Autoplay({ delay: 3000 })]);
 
 const removeDotBtnsAndClickHandlers = addDotBtnsAndClickHandlers(
-	emblaApi, dotsNode, currentSlideCounterNode
+  emblaApi,
+  dotsNode,
+  currentSlideCounterNode
 );
 
-emblaApi.on('destroy', removeDotBtnsAndClickHandlers);
-
+emblaApi.on("destroy", removeDotBtnsAndClickHandlers);
 
 async function selectStatisticsTimeframe(_, eventTrigger) {
-	const currentButton = document.querySelector('.statistics-timeframe[data-active="true"]');
-	const currentView = document.querySelector(`.statistics-view[data-value="${currentButton.dataset.value}"]`);
+  const currentButton = document.querySelector(
+    '.statistics-timeframe[data-active="true"]'
+  );
+  const currentView = document.querySelector(
+    `.statistics-view[data-value="${currentButton.dataset.value}"]`
+  );
 
-	const newButton = eventTrigger;
-	const newView = document.querySelector(`.statistics-view[data-value="${newButton.dataset.value}"]`);
+  const newButton = eventTrigger;
+  const newView = document.querySelector(
+    `.statistics-view[data-value="${newButton.dataset.value}"]`
+  );
 
-	if (currentButton.dataset.value === newButton.dataset.value) return;
+  if (currentButton.dataset.value === newButton.dataset.value) return;
 
-	currentButton.dataset.active = false;
-	await leave(currentView, 'fade');
-	currentView.ariaHidden = true;
+  currentButton.dataset.active = false;
+  await leave(currentView, "fade");
+  currentView.ariaHidden = true;
 
-	newButton.dataset.active = true;
-	await enter(newView, 'fade');
-	newView.ariaHidden = false;
+  newButton.dataset.active = true;
+  await enter(newView, "fade");
+  newView.ariaHidden = false;
 }
 
-listen('click', '.statistics-timeframe[data-value="all"]', selectStatisticsTimeframe);
-listen('click', '.statistics-timeframe[data-value="today"]', selectStatisticsTimeframe);
-listen('click', '.statistics-timeframe[data-value="yesterday"]', selectStatisticsTimeframe);
-listen('click', '.statistics-timeframe[data-value="week"]', selectStatisticsTimeframe);
-listen('click', '.statistics-timeframe[data-value="month"]', selectStatisticsTimeframe);
+listen(
+  "click",
+  '.statistics-timeframe[data-value="all"]',
+  selectStatisticsTimeframe
+);
+listen(
+  "click",
+  '.statistics-timeframe[data-value="today"]',
+  selectStatisticsTimeframe
+);
+listen(
+  "click",
+  '.statistics-timeframe[data-value="yesterday"]',
+  selectStatisticsTimeframe
+);
+listen(
+  "click",
+  '.statistics-timeframe[data-value="week"]',
+  selectStatisticsTimeframe
+);
+listen(
+  "click",
+  '.statistics-timeframe[data-value="month"]',
+  selectStatisticsTimeframe
+);
 
-const exchangeRatesSection = document.getElementById('exchange-rates-section');
-const exchangeRatesContainer = document.getElementById('exchange-rates-container');
+const exchangeRatesSection = document.getElementById("exchange-rates-section");
+const exchangeRatesContainer = document.getElementById(
+  "exchange-rates-container"
+);
 const exchangeRatesAnimation = new SwapAnimation(exchangeRatesContainer, 1200);
-exchangeRatesSection.addEventListener('pointerenter', () => { exchangeRatesAnimation.run() });
-exchangeRatesSection.addEventListener('pointerleave', () => { exchangeRatesAnimation.stop() });
+exchangeRatesSection.addEventListener("pointerenter", () => {
+  exchangeRatesAnimation.run();
+});
+exchangeRatesSection.addEventListener("pointerleave", () => {
+  exchangeRatesAnimation.stop();
+});
 
-const transfersSection = document.getElementById('transfers-section');
-const transfersContainer = document.getElementById('transfers-container');
+const transfersSection = document.getElementById("transfers-section");
+const transfersContainer = document.getElementById("transfers-container");
 const transfersAnimation = new FlowAnimation(transfersContainer, 2000);
-transfersSection.addEventListener('pointerenter', () => { transfersAnimation.run() });
-transfersSection.addEventListener('pointerleave', () => { transfersAnimation.stop() });
+transfersSection.addEventListener("pointerenter", () => {
+  transfersAnimation.run();
+});
+transfersSection.addEventListener("pointerleave", () => {
+  transfersAnimation.stop();
+});
 
-const anonPaymentsSection = document.getElementById('anon-payments-section');
-const anonPaymentsContainer = document.getElementById('anon-payments-container');
+const anonPaymentsSection = document.getElementById("anon-payments-section");
+const anonPaymentsContainer = document.getElementById(
+  "anon-payments-container"
+);
 const anonPaymentsAnimation = new FlowAnimation(anonPaymentsContainer, 2000);
-anonPaymentsSection.addEventListener('pointerenter', () => { anonPaymentsAnimation.run() });
-anonPaymentsSection.addEventListener('pointerleave', () => { anonPaymentsAnimation.stop() });
-
-
+anonPaymentsSection.addEventListener("pointerenter", () => {
+  anonPaymentsAnimation.run();
+});
+anonPaymentsSection.addEventListener("pointerleave", () => {
+  anonPaymentsAnimation.stop();
+});
